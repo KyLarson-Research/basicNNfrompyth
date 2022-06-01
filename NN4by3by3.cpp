a0dd050f2a569ceba5a7f5c26a53237061d79d15
KyLarson-Research <kylel@email.arizona.edu>
2022-06-01 15:11:45 -0700

    dropped complexity added more prints

--- NN4by3by3.cpp
+++ NN4by3by3.cpp
@@ -1,8 +1,11 @@
 // NN4by3by3.cpp : Authored 5-20-21 by Kyle Larson and Kyle Savery for Proto6909
 //TODO insert license here
 #include <iostream>
-int dot(int* A, int* B, int n) {
-    int dot = 0;
+#include <time.h> //time
+#include <stdlib.h> //rand
+
+float dot(float* A, float* B, int n) {
+    float dot = 0;
 #
     for (int i = 0; i < n; i++) {
         dot += A[i] * B[i];
@@ -14,27 +17,88 @@ float sigmoid(float x) {
     return 1 / (1 + exp(-x));//may be a good spot to try some computations using exp to compare to python
 }// e^x as in euler's function not some exponent but sometimes 2^x is substituted?
 
-float sigmoid_derivative(float x) {
-    return x * (1 - x);
+float working_error() {
+
 }
+
+float sigmoid_derivative(float x) {
+    float temp = sigmoid(x);
+    return temp * (1 - temp);
+}
+float sq(float x) { return x * x; }
+float mean_sq_err(float *e, float *o, int n) {
+    float sum = 0;
+    for (int i = 0; i < n; i++) { sum += sq( o[i] - e[i] ); }// sum squared error
+    return sum / n;
+}
+
+
 int main()
 {
-    int test_inputs[16][4] = {
-        {0, 0, 0, 0}, { 0, 0, 0, 1 }, { 0, 0, 1, 0 }, { 0, 0, 1, 1 }, { 1, 1, 0, 0 }, { 1, 1, 0, 1 }, { 1, 1, 1, 0 }, { 1, 1, 1, 1 },
-        {0,1,0,0}, {0,1,0,1}, {0,1,1,0}, {0,1,1,1}, {1,0,0,0}, {1,0,0,1}, {1,0,1,0}, {1,0,1,1} };
+    
 
-    int test_outputs[8][3] = { {0, 0, 1}, {0, 0, 0}, {1, 0, 1}, {0, 1, 1}, {1, 1, 0}, {1, 0, 0}, {1, 1, 1}, {0, 1, 0 } };
+    int test_inputs[4][2] = { {-1,-1}, {-1,1}, {1,-1}, {1,1} };
+
+        /*{ {0, 0, 0, 0}, { 0, 0, 0, 1 }, { 0, 0, 1, 0 }, 
+        { 0, 0, 1, 1 }, { 1, 1, 0, 0 }, { 1, 1, 0, 1 }, 
+        { 1, 1, 1, 0 }, { 1, 1, 1, 1 }, { 0, 1, 0, 0 }, 
+        { 0, 1, 0, 1 }, { 0, 1, 1, 0 }, { 0, 1, 1, 1 }, 
+        { 1, 0, 0, 0 }, { 1, 0, 0, 1 }, { 1, 0, 1, 0 }, { 1, 0, 1, 1} };*/
+
+    int test_outputs[4][1] = { 1, -1, -1, 1 };
+        /*{ {0, 0, 1}, {0, 0, 0}, {1, 0, 1}, 
+        {0, 1, 1}, {1, 1, 0}, {1, 0, 0},
+        {1, 1, 1}, {0, 1, 0 } };*/
+    // initial quanities
     int epochs = 100000;
     float lr = 0.001;
-    int inputLayerNeurons = 4;
-    int hiddenLayerNeurons = 4;
-    int outputLayerNeurons = 3;
-    std::cout << "\ninput neurons" << inputLayerNeurons << "\nhidden neurons" << hiddenLayerNeurons << "\noutput neurons" << outputLayerNeurons;
-    //Random weightsand bias initialization
-    int   hidden_weights = np.random.uniform(size = (inputLayerNeurons, hiddenLayerNeurons))
-        hidden_bias = np.random.uniform(size = (1, hiddenLayerNeurons))
-        output_weights = np.random.uniform(size = (hiddenLayerNeurons, outputLayerNeurons))
-        output_bias = np.random.uniform(size = (1, outputLayerNeurons))
+    int inputLayerNeurons = 2; 
+    int hiddenLayerNeurons = 2;
+    int outputLayerNeurons = 1;
+    
+    //int
+    float i_1 = test_inputs[0][0];
+    float i_2 = test_inputs[0][1];
+    float I[2] = { i_1, i_2 };
+    //Random weights and bias initialization
+    //initialize random
+    srand(time(NULL));
+    float w_1 = ((float)rand() / RAND_MAX) * 2 - 1; //Generate random numbers between -1 and 1
+    float w_2 = ((float)rand() / RAND_MAX) * 2 - 1;
+    float w_3 = ((float)rand() / RAND_MAX) * 2 - 1;
+    float w_4 = ((float)rand() / RAND_MAX) * 2 - 1;
+    float w_5 = ((float)rand() / RAND_MAX) * 2 - 1;
+    float w_6 = ((float)rand() / RAND_MAX) * 2 - 1;
+
+    float u[2] = { w_1, w_2 };
+    float v[2] = { w_3, w_4 };
+    float w[2] = { w_5, w_6 };
+
+    // initial forward propigation
+
+    float h_1 = sigmoid(dot(u, I, 2));
+    float h_2 = sigmoid(dot(v, I, 2));
+    float h[2] = { h_1, h_2 };
+    float out = sigmoid(dot(h, w, 2));
+
+    // back propigate 
+   // float working_error = 
+
+    std::cout 
+        << "\ninput: " << i_1 << " " << i_2
+        << "\nn input neurons: " << inputLayerNeurons
+        << "\nhidden input weight: " << w_1 << "  " << w_2 << "  " << w_3 << "  " << w_4
+        << "\nn hidden neurons: " << hiddenLayerNeurons
+        << "\nhidden neuron potentials: " << h_1 << " " << h_2
+        << "\nhidden output weight: " << w_5 << "  " << w_6 
+        << "\nn output neurons: " << outputLayerNeurons
+        << "\noutput: " << out
+        << "\n\n";
+
+    
+
+    //int   hidden_weights = 
+        
     return 0;
 }
 //________________SECTION FOR PORTING FROM PYTHON TO THE ABOVE
